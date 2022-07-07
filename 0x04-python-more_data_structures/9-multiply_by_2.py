018c621656a5a391c4326bd55433e4e160fa55b9
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-07-06 19:53:27 -0700

    Multiply new by 2 commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
+    new_ones = a_dictionary.copy()
+    list_keys = list(new_ones.keys())
 
     for i in list_keys:
         new_ones[i] = new_ones[i] * 2
