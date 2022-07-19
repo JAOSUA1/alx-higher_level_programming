89a8daeed769cc2cca30c325999a2412170a79a6
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-07-18 23:07:47 -0700

    List exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+
     count = 0
 
     for i in range(x):
@@ -8,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
         except:
             break
         else:
-            count += 1
+            count = count + 1
 
     print()
     return (count)
